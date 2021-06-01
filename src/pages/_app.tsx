0c56a52d890b8fd2135dc8971b34518c3a1700eb
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-06-01 09:09:00 -0300

    utilizando thema customizado

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,14 @@
 import { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
+
+import { theme } from '../styles/theme'
 import { Header } from '../components/Header/indes'
+
+
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Header />
       <Component {...pageProps} />
     </ChakraProvider>
