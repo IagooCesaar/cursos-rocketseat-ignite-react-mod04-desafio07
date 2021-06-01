5ebc4df8ed6d7a924a7bc9ec08eac608ca1f7fa1
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-06-01 19:17:33 -0300

    Divider

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { Flex } from '@chakra-ui/react';
+import { Flex, Box, Divider } from '@chakra-ui/react';
 import { Banner } from '../components/Banner'
 import { TravelTypes } from '../components/TravelTypes';
 
@@ -7,6 +7,12 @@ export default function Home() {
     <Flex w='100%' direction='column'>
       <Banner />
       <TravelTypes />
+      <Box flex='1' width={['60px', '90px']} marginX='auto' my="20">
+        <Divider
+          border='2px'
+          borderColor='body.color'
+        />
+      </Box>
       Continuação...
     </Flex>
   )
