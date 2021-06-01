5a01b585f8009d4293ae448a5e4f36be26b260aa
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-06-01 10:24:18 -0300

    adicionando logo ao cabeçalho

--- src/components/Header/indes.tsx
+++ src/components/Header/indes.tsx
@@ -1,5 +1,16 @@
+import { Flex, Image } from '@chakra-ui/react';
+
 export function Header() {
   return (
-    <h1>Header</h1>
+    <Flex
+      as='header'
+      w='100%'
+      h='100px'
+      marginX='auto'
+      align='center'
+      justifyContent='center'
+    >
+      <Image src='/logo.svg' alt='WorldTrip' objectFit='cover' h='12' />
+    </Flex>
   )
 }
