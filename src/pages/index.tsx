be5f1999a07990c18da73b32c21069c12b088e46
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-06-01 15:37:48 -0300

    Iniciando Banner

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,10 @@
 import { Flex } from '@chakra-ui/react';
+import { Banner } from '../components/Banner'
 
 export default function Home() {
   return (
     <Flex w='100%'>
-      página inicial
+      <Banner />
     </Flex>
   )
 }
