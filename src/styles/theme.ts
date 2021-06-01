18a4cdf19327261ca344a28b0fd0c016da354bc2
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-06-01 10:24:07 -0300

    cores do Figma

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -2,7 +2,7 @@ import { extendTheme, ThemeConfig } from '@chakra-ui/react'
 
 const config: ThemeConfig = {
   initialColorMode: 'light',
-  useSystemColorMode: false,
+  useSystemColorMode: false,  
 }
 
 export const theme = extendTheme({
@@ -10,5 +10,28 @@ export const theme = extendTheme({
   fonts: {
     heading: 'Poppins',
     body: 'Poppins',
+  },
+  colors: {
+    default: {
+      'highligh': '#ffba08',
+      'white': '#ffffff',
+      'black': '#000000',
+      'dark': {
+        'text': '#47585b',
+        'info': '#999999'
+      },
+      'light': {
+        'text': '#f5f8fa',
+        'info': '#dadada'
+      }
+    }
+  },
+  styles: {
+    global: {
+      body: {
+        bg: 'default.light.text',
+        color: 'default.dark.text',
+      }
+    }
   }
 })
