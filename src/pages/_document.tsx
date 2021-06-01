bcf8e320d114450962b2624697143662d4e22d7c
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-06-01 08:45:25 -0300

    add Favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,6 +6,7 @@ export default class MyDocument extends Document {
       <Html>
         <Head>
 
+          <link rel="shortcut icon" href="favicon.png" type="image/png" />
           <title>WorldTrip</title>
         </Head>
         <body>
