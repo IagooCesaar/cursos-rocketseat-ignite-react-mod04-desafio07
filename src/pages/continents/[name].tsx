8402837efdc9b359d06da9f9d9039d7e76e46e5d
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-06-02 19:30:04 -0300

    obtendo dados da API para gerar detalhes da página do continente

--- src/pages/continents/[name].tsx
+++ src/pages/continents/[name].tsx
@@ -1,12 +1,44 @@
+import { GetStaticProps, GetStaticPaths } from 'next';
 import { Flex } from '@chakra-ui/react';
+import { api } from '../../services/api';
 
-export default function Continents() {
+interface ContinentsProps {
+  continent: {
+    id: string;
+    title: string;
+    description: string;
+    image: string;
+    betterImagePosition: string;
+  }
+}
+
+export default function Continents({
+  continent,
+}: ContinentsProps) {
   return (
     <Flex w='100%' direction='column' pb='12'>
       <Flex maxW='1240px' px='10'>
-        <h1>Continentes</h1>
+        <h1>{continent.title}</h1>
 
       </Flex>
     </Flex>
   )
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [],
+    fallback: 'blocking',
+  }
+}
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const { name } = params;
+  const response = await api.get(`/continents/${String(name)}`)
+  const continent = response.data;
+  return {
+    props: {
+      continent
+    }
+  }
 }
