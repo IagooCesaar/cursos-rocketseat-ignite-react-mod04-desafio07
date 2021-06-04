7ba04960f1fb770d2ce484b60cfea8796ee8f297
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-06-04 16:43:05 -0300

    início dados da página do continente

--- src/pages/continents/[name].tsx
+++ src/pages/continents/[name].tsx
@@ -1,8 +1,14 @@
-import { GetStaticProps, GetStaticPaths } from 'next';
-import { Flex } from '@chakra-ui/react';
-import { api } from '../../services/api';
 import React from 'react';
+import { GetStaticProps, GetStaticPaths } from 'next';
+import {
+  Box,
+  Flex,
+  Text,
+  useBreakpointValue,
+} from '@chakra-ui/react';
+
 import { ContinentBanner } from '../../components/ContinentBanner';
+import { api } from '../../services/api';
 
 interface ContinentsProps {
   continent: {
@@ -17,6 +23,10 @@ interface ContinentsProps {
 export default function Continents({
   continent,
 }: ContinentsProps) {
+  const isWideVersion = useBreakpointValue({
+    base: false,
+    lg: true,
+  })
   return (
     <Flex w='100%' direction='column' pb='12'>
       <ContinentBanner
@@ -25,10 +35,57 @@ export default function Continents({
         bgSize='cover'
         continentName={continent.title}
       />
-      <Flex maxW='1240px' px='10'>
-        <h1>{continent.title}</h1>
-      </Flex>
-    </Flex>
+      <Box maxW='1240px' px='10' marginX='auto'>
+        <Flex
+          justify='space-between'
+          align='center'
+          width='100%'
+          direction={isWideVersion ? 'row' : 'column'}
+        >
+          <Box flex='1'>
+            <Text as='p' fontSize={['md', 'lg']} align='justify'>
+              A Europa é, por convenção, um dos seis continentes do mundo. Compreendendo
+              a península ocidental da Eurásia. A Europa geralmente divide-se da Ásia a leste pela
+              divisória de águas dos montes Urais, o rio Ural, o mar Cáspio, o Cáucaso, e o
+              mar Negro a sudes
+            </Text>
+          </Box>
+          <Flex
+            flex='1'
+            direction='row'
+            justify="space-around"
+            align="center"
+          >
+            <Flex
+              direction='column'
+              fontSize={['1xl', '2xl']}
+              align='center'
+            >
+              <Text fontSize={['20', '38']} fontWeight='semibold' color='default.highlight'>50</Text>
+              <Text fontWeight='semibold'>países</Text>
+            </Flex>
+            <Flex
+              direction='column'
+              fontSize={['1xl', '2xl']}
+              justify="center"
+              align='center'
+            >
+              <Text fontSize={['20', '38']} fontWeight='semibold' color='default.highlight'>60</Text>
+              <Text fontWeight='semibold'>línguas</Text>
+            </Flex>
+            <Flex
+              direction='column'
+              fontSize={['1xl', '2xl']}
+              justify="center"
+              align='center'
+            >
+              <Text fontSize={['20', '38']} fontWeight='semibold' color='default.highlight'>27</Text>
+              <Text fontWeight='semibold'>cidades</Text>
+            </Flex>
+          </Flex>
+        </Flex>
+      </Box>
+    </Flex >
   )
 }
 
